bed820913f79f8ef845ae50fd47c9ae00bd1c1df
Marco Antonio Branco Filho <marcoantoniobrancofilho@hotmail.com>
2023-09-30 10:46:40 -0300

    web_development DEV: Created InvoiceExtraction class and all sub fuctions

--- WebDev.py
+++ WebDev.py
@@ -1,14 +1,69 @@
 from selenium import webdriver
-import os
+from selenium.webdriver.common.by import By
 
 class SeleniumActions:
+        
     def startBrowser(self):
-        currentPath = os.getcwd()
-        self.driver = webdriver.Chrome(executable_path="")
-        self.driver.get("https://www.google.com/")
-        print("TESTE")
+        self.driver = webdriver.Chrome()
+
+    def OpenUrl(self, webUrl):
+        self.driver.get(webUrl)
+
+    def FindElement(self, xpath):
+        element = self.driver.find_element(By.XPATH, xpath)
+        return element
+    
+    def FindElements(self, xpath):
+        element = self.driver.find_elements(By.XPATH, xpath)
+        return element
+    
+class InvoiceExtraction:
+
+    def __init__(self):
+        self.elementsDic = {"mainTable" :       "//table[@id='tableSandbox']",
+                            "pageButtons":      "//a[@class='paginate_button ']",
+                            "pageButton" :      "//a[@data-dt-idx='{}']"}
+        
+        self.sel = SeleniumActions()
+
+    def GetElement(self, element, formatValue = ""):
+        if formatValue != "":
+            return self.elementsDic[element].format(formatValue)
+    
+        return self.elementsDic[element]
+        
+
+    def Start(self, webUrl):
+        self.sel.startBrowser()
+        self.sel.OpenUrl(webUrl)
+        self.GetTableValues()
 
     
+    def CheckTotalPages(self):
+        pageButtons = self.sel.FindElements(self.GetElement("pageButtons"))
 
-teste = SeleniumActions()
-teste.startBrowser()
+        buttonText = pageButtons[-1].text
+
+        return int(buttonText)
+
+    
+    def GetTableValues(self):
+        tableElement = self.sel.FindElement(self.GetElement("mainTable"))
+        tableValues = {}
+        pageNumber = self.CheckTotalPages()
+        for i in range(1, pageNumber+1):
+            for row in tableElement.find_elements(By.TAG_NAME, "tr"):
+                rowText = row.text.split()
+                if rowText[0] == "#": continue
+                tableValues[rowText[0]] = rowText[1:]
+
+            if i == pageNumber:
+                pageButton = self.sel.FindElement(self.GetElement("pageButton", formatValue=i))
+
+            pageButton = self.sel.FindElement(self.GetElement("pageButton", formatValue=i+1))
+            pageButton.click()
+        
+        return tableValues
+    
+teste = InvoiceExtraction()
+teste.Start("https://rpachallengeocr.azurewebsites.net/")
